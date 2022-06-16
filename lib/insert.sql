c62d08bcb4d5cb9cd1d6c7e0a9addb300392bc64
Kenyansa <kenyansajustine@gmail.com>
2022-06-16 05:14:50 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 4, "M", "black", "noisy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "black", "calm", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 3, "F", "grey", "violent", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 12, "M", "white", "restless", 1);
+INSERT INTO bears VALUES (5, "Melissa", 2, "F", "white_black-Spots", "friendly", 1);
+INSERT INTO bears VALUES (6, "Grinch", 5, "M", "brown", "violent", 0);
+INSERT INTO bears VALUES (7, "Wendy", 7, "F", "grey-black", "noisy", 1);
+INSERT INTO bears VALUES (8, NULL, 8, "M", "white", "friendly", 1);
